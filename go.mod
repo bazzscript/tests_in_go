020476446e894ab39aacfb633ea2a57af82c8464
bazzscript <bezaleelnwabia@gmail.com>
2023-12-03 12:18:33 +0100

    completed wordcount exercise

--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module github.com/bazzscript/tests_in_go
 
 go 1.18
 
+require golang.org/x/tour v0.1.0
---
+++ go_tour_exercises/wordcount.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	// Create an empty map to store the count of each word.
+	result := make(map[string]int)
+
+	// Split the string into words using strings.Fields.
+	// This handles splitting on spaces and also cleans up any leading or trailing spaces.
+	words := strings.Fields(s)
+
+	// Iterate over each word.
+	for _, word := range words {
+		// Increase the count for each word in the map.
+		// The map will automatically handle new words.
+		result[word]++
+	}
+
+	// Return the map with word counts.
+	return result
+}
+
+func main() {
+	wc.Test(WordCount)
+}
